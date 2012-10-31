2e94cbb21b5be4388ef89b9150237ed17793bdd7
Justin Beckwith <justin.beckwith@gmail.com>
2012-10-31 00:58:48 -0700

    d

--- KuduScripts/node/deploy.js
+++ KuduScripts/node/deploy.js
@@ -60,27 +60,8 @@ function minify(callback) {
  */
 function copy(callback) {
 	console.log('Copying files from ' + deploymentTemp + ' to ' + deploymentTarget);
-	//wrench.copyDirSyncRecursive(deploymentTemp, deploymentTarget);
-
-	var xcopycommand = "xcopy \"" + deploymentTemp + "\" \"" + deploymentTarget + "\" /Y /Q /E";
-	var xcopy = child_process.exec(xcopycommand, function(error, stdout, stderr) {
-		if(error) {
-			console.log(error.stack);
-			console.log('Error code: ' + error.code);
-			console.log('Signal received: ' + error.signal);			
-			process.exit(1);
-		}
-		if(stderr) console.error(stderr);
-		if(stdout) console.log(stdout);	
-	});
-
-	xcopy.on('exit', function(code) {
-		console.log('xcopy exited with exit code ' + code);
-		callback();
-	});	
-
-
-	//callback();
+	wrench.copyDirSyncRecursive(deploymentTemp, deploymentTarget);
+	callback();
 }
 
 /*
